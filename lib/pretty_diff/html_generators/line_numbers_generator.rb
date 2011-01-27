26a68ed43f092c5665da94745b062a82f3cc8b42
Ilya Sabanin <ilya.sabanin@gmail.com>
2011-01-27 15:12:56 +0700

    Adjusting line numbers format.

--- lib/pretty_diff/html_generators/line_numbers_generator.rb
+++ lib/pretty_diff/html_generators/line_numbers_generator.rb
@@ -13,16 +13,25 @@ class PrettyDiff::LineNumbersGenerator
 private
 
   def left_column
-    line_numbers.left_column.join("\n")
+    empty_lines_to_whitespace(line_numbers.left_column).join("\n")
   end
 
   def right_column
-    line_numbers.right_column.join("\n")
+    empty_lines_to_whitespace(line_numbers.right_column).join("\n")
+  end
+  
+  def empty_lines_to_whitespace(numbers)
+    result = []
+    numbers.each do |v|
+      result << (v.nil? ? '&nbsp;' : v)
+    end
+    result
   end
 
   def column_html(text)
     %Q[<div class="code-lines"><pre>
-#{text}</pre></div>]
+#{text}
+</pre></div>]
   end
 
 end
