2fce6a950471767cfae4a99c2fb2059d9a6b4858
Ilya Sabanin <ilya.sabanin@gmail.com>
2011-12-07 15:57:53 -0500

    We don't use that code.

--- lib/pretty_diff/html_generators/diff_generator.rb
+++ lib/pretty_diff/html_generators/diff_generator.rb
@@ -7,22 +7,7 @@ class PrettyDiff::DiffGenerator
   end
 
   def generate
-    chunks_html = diff.chunks.map{|c| c.to_html}.join(chunk_separator_html).to_s
-    intro_html + chunks_html + outro_html
-  end
-
-private
-
-  def intro_html
-    %Q[]
-  end
-
-  def chunk_separator_html
-    %Q[]
-  end
-
-  def outro_html
-    %Q[]
+    diff.chunks.map{|c| c.to_html}.to_s
   end
 
 end
