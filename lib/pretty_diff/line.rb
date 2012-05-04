c2bf9bcabce3c0d66374cef3b4e4c6490bb4a61c
Ilya Sabanin <ilya.sabanin@gmail.com>
2012-05-04 10:58:50 -0400

    Revert "Support wdiff format."
    This reverts commit 1ba45fdd91ba13f2af566f2a3462364195709f14.

--- lib/pretty_diff/line.rb
+++ lib/pretty_diff/line.rb
@@ -3,11 +3,6 @@
 #
 class PrettyDiff::Line #:nodoc:
 
-  INLINE_INSERT_START = /\{\+/
-  INLINE_INSERT_END = /\+\}/
-  INLINE_DELETE_START = /\[-/
-  INLINE_DELETE_END = /-\]/
-
   attr_reader :diff, :input
 
   def initialize(diff, input)
@@ -22,11 +17,7 @@ class PrettyDiff::Line #:nodoc:
 
   # Prepare Line contents for injection into HTML structure.
   def format
-    input.gsub(INLINE_INSERT_START, '<ins>') \
-      .gsub(INLINE_INSERT_END, '</ins>') \
-      .gsub(INLINE_DELETE_START, '<del>') \
-      .gsub(INLINE_DELETE_END, '</del>') \
-      .gsub("\t", '  ')
+    input.gsub("\t", '  ')
   end
 
   # Unified Diff sometimes emit a special line at the end of the file
@@ -36,11 +27,9 @@ class PrettyDiff::Line #:nodoc:
     input =~ /\\ No newline at end of file/
   end
 
-  # Return status of the Line. Can be :modified (for per-word diffing), :added, :deleted or :not_modified.
+  # Return status of the Line. Can be :added, :deleted or :not_modified.
   def status
     case input
-    when /(#{ INLINE_DELETE_START }|#{ INLINE_INSERT_START })/
-      :modified
     when /^\+/
       :added
     when /^\-/
@@ -48,10 +37,6 @@ class PrettyDiff::Line #:nodoc:
     else
       :not_modified
     end
-  end
-
-  def modified?
-    status == :modified
   end
 
   def added?
