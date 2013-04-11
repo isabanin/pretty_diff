b5b149a186c23e5c537a2776b45cce3dc996196a
Ilya Sabanin <ilya@sabanin.ru>
2013-04-11 09:15:50 -0400

    Oops

--- test/basic_generator_test.rb
+++ test/basic_generator_test.rb
@@ -4,7 +4,6 @@ class BasicGeneratorTest < MiniTest::Unit::TestCase
 
   def test_generated_html
     diff = new_diff(fixture('first.diff'), :generator => PrettyDiff::BasicGenerator)
-    File.open('/Users/ilya/Desktop/zopa', 'w'){|f| f << diff.to_html}
     assert_equal fixture('first.diff.html'), diff.to_html
   end
 
